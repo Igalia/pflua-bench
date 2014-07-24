7844883c308b62735a35caed9be9e34e7e05c79e
Javier M. Mellid <jmunhoz@igalia.com>
2014-07-24 14:43:29 +0200

    Add linuxjit engine in bench.lua

--- src/bench.lua
+++ src/bench.lua
@@ -27,8 +27,44 @@ struct timeval {
 int gettimeofday(struct timeval *tv, struct timezone *tz);
 ]]
 
+ffi.cdef[[
+typedef void (*compile_filter_t)(char *f);
+typedef int (*run_filter_on_packet_t)(uint32_t pkt_len, const uint8_t *pkt);
+void *dlopen(const char *filename, int flag);
+void *dlsym(void *handle, const char *symbol);
+char *dlerror(void);
+]]
+
+local lib_handle
+-- void compile_filter(char *f);
+local kernel_compile_filter
+-- int run_filter_on_packet(uint32_t pkt_len, const uint8_t *pkt);
+local kernel_run_filter_on_packet
+
 local zero_sec, zero_usec
 
+function load_dyn_funcs()
+   local RTLD_LAZY = 0x0001
+   lib_handle = ffi.C.dlopen("./ref/bpf-jit-kernel/libbpf_jit_kernel.so.1.0.0", RTLD_LAZY)
+   if lib_handle == nil then
+      print(ffi.C.dlerror())
+      print("did you compile .so library? it should be at ./ref/bpf-jit-kernel/libbpf_jit_kernel.so.1.0.0 ...")
+      print("try 'make -C ref/bpf-jit-kernel lib'")
+      os.exit(-1)
+   end
+   kernel_compile_filter = ffi.cast("compile_filter_t", ffi.C.dlsym(lib_handle, "compile_filter"));
+   kernel_run_filter_on_packet = ffi.cast("run_filter_on_packet_t", ffi.C.dlsym(lib_handle, "run_filter_on_packet"));
+end
+
+function convert_filter_to_dec_numbers(str)
+   local line = ""
+   local cmd = "/usr/sbin/tcpdump -ddd -r ts/pcaps/igalia/empty.pcap '" .. str .. "' 2> /dev/null | tr '\n' ','"
+   local io = assert(io.popen(cmd, 'r'))
+   line = io:read()
+   line = line:sub(1,#line-1)
+   io.close()
+   return ffi.new("char[?]", #line, line)
+end
 local function now()
    local tv = ffi.new("struct timeval")
    assert(ffi.C.gettimeofday(tv, nil) == 0)
@@ -51,6 +87,12 @@ local function compile_filter(filter_str, opts)
       local bytecode = libpcap.compile(filter_str, dlt)
       local bpf_prog = bpf.compile(bytecode)
       return function(P, header, len) return bpf_prog(P, len) ~= 0 end
+   elseif opts.linux_jit then
+      load_dyn_funcs()
+      local bytecode = convert_filter_to_dec_numbers(filter_str)
+      kernel_compile_filter(bytecode)
+      return function(P, header, len)
+         return kernel_run_filter_on_packet(len, P) ~= 0 end
    else
       local expr = parse.parse(filter_str)
       expr = expand.expand(expr, dlt)
@@ -72,6 +114,7 @@ for line in io.lines(capture..'.tests') do
       filter=filter,
       libpcap=compile_filter(filter, {pcap_offline_filter=true}),
       bpf=compile_filter(filter, {bpf=true}),
+      linuxjit=compile_filter(filter, {linux_jit=true}),
       pflua=compile_filter(filter)
    }
    table.insert(tests, test)

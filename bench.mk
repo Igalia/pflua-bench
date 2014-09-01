bd282e3bdd56d2a3db58c077d51eabcc6be9e143
Andy Wingo <wingo@igalia.com>
2014-09-01 17:11:55 +0200

    Update and regenerate with more iterations for wingolog tests
    * bench.mk: Add capability to set iterations count.
    * results/wingolog.org-1/Makefile:
    * results/wingolog.org-2/Makefile: Up iterations counts for these short
      tests.  Regenerate results.
    
    * deps/pflua: Update.

--- bench.mk
+++ bench.mk
@@ -3,6 +3,7 @@ GUILE=guile
 PATH:=$(top_srcdir)/deps/pflua/deps/luajit/usr/local/bin:$(PATH)
 ENGINES:=libpcap linux-bpf linux-ebpf bpf-lua pflua
 CSV:=$(addsuffix .csv, $(ENGINES))
+ITERATIONS?=50
 
 all: $(PNG)
 
@@ -12,7 +13,7 @@ maintainer-clean:
 	rm -f $(CSV) $(PNG)
 
 %.csv: $(top_srcdir)/bench.lua $(SAVEFILE) filters
-	luajit $(top_srcdir)/bench.lua $(SAVEFILE) filters $* > $@.tmp
+	luajit $(top_srcdir)/bench.lua $(SAVEFILE) filters $* $(ITERATIONS) > $@.tmp
 	mv $@.tmp $@
 
 $(PNG): Makefile $(top_srcdir)/bench.mk $(CSV)
